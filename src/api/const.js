ed266480a0eb2f5e9655ec0a38f2490bc8993ba7
anna <unursey@gmail.com>
2022-11-29 01:30:14 +0300

    merge

--- src/api/const.js
+++ src/api/const.js
@@ -3,5 +3,5 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = 'T8PhqIooA8055lEC0n9JxA';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
-export const REDIRECT_URI = 'http://localhost:3000/auth';
+export const REDIRECT_URI = 'https://blogget-self.vercel.app/auth';
 export const SCOPE = 'identity submit read';
